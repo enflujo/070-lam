69dab86658b0d66671050fdbc9f80d337ef0a07c
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-06-01 07:32:30 -0500

    🚧 Boceto pasar nodos a cerebro

--- aplicaciones/www/src/cerebros/general.ts
+++ aplicaciones/www/src/cerebros/general.ts
@@ -1,8 +1,10 @@
-import { atom } from 'nanostores';
+import { atom, map } from 'nanostores';
 import { DatosAgente } from '../tipos';
+import Nodo from '../componentes/Nodo';
 
 export const leyendo = atom<boolean>(false);
 export const mostrarAgente = atom<DatosAgente | null>(null);
 export const estanOrbitando = atom<boolean>(true);
 export const tipoAgente = atom<'persona' | 'org' | 'lam'>('lam');
 export const agenteActivo = atom<string | null>(null);
+export const nodos = map<Nodo>();
--- aplicaciones/www/src/componentes/Nodo.ts
+++ aplicaciones/www/src/componentes/Nodo.ts
@@ -2,6 +2,7 @@ import { agenteActivo, estanOrbitando, mostrarAgente } from '../cerebros/general
 import type { DatosAgente, Dims, Punto } from '../tipos';
 
 const DOS_PI = Math.PI * 2;
+const conexiones = document?.querySelector<SVGGElement>('#conexiones');
 
 export default class Nodo {
   grupo: HTMLDivElement;
@@ -11,6 +12,10 @@ export default class Nodo {
   radio: number;
   centro: Punto;
   activo: boolean;
+  x: number;
+  y: number;
+  lineas: SVGLineElement[];
+  mostrarRelaciones: boolean;
 
   constructor(contenedor: HTMLDivElement, datos: DatosAgente, anillo: number, angulo: number, dims: Dims) {
     this.grupo = document.createElement('div');
@@ -22,6 +27,10 @@ export default class Nodo {
     this.centro = { x: 0, y: 0 };
     this.angulo = angulo;
     this.activo = true;
+    this.x = 0;
+    this.y = 0;
+    this.lineas = [];
+    this.mostrarRelaciones = false;
 
     texto.className = 'nombre';
     texto.innerText = datos.nombre;
@@ -40,17 +49,29 @@ export default class Nodo {
       if (!this.activo) return;
       estanOrbitando.set(false);
       mostrarAgente.set(this.datos);
+      this.mostrarRelaciones = true;
     };
 
     this.grupo.onmouseleave = () => {
       if (!this.activo) return;
       estanOrbitando.set(true);
+      this.mostrarRelaciones = false;
     };
 
     this.grupo.onclick = (evento) => {
       evento.stopPropagation();
       agenteActivo.set(this.datos.nombre);
+      this.mostrarRelaciones = true;
     };
+
+    if (datos.relaciones.length) {
+      datos.relaciones.forEach((relacion) => {
+        const linea = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        linea.setAttribute('class', `conexion ${relacion.tipoRelacion}`);
+        linea.setAttribute('data-con', relacion.con);
+        conexiones?.appendChild(linea);
+      });
+    }
   }
 
   escalar(dims: Dims) {
@@ -67,6 +88,8 @@ export default class Nodo {
     const y = this.centro.y + _y;
 
     this.grupo.style.transform = `translate(${x}px,${y}px)`;
+    this.x = x;
+    this.y = y;
   }
 
   activar() {
